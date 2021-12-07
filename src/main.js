7defee883274b18aa1e92f5437b0a3ecf7c726f9
sallypan9999 <qqcandy11619@gamil.com>
2021-12-07 15:09:41 +0800

    購物車

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faShoppingBag, faShoppingCart, faInfoCircle, faCartArrowDown, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+Vue.use(VueSweetalert2)
+library.add(faShoppingBag, faShoppingCart, faInfoCircle, faCartArrowDown, faTimes)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
